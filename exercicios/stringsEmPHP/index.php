aa90e577feeaa78d545bda0a213495b436e5f1fd
Emerson pinho <the.emerson.araujo@gmail.com>
2024-11-04 11:20:02 -0300

    commit

---
+++ exercicios/stringsEmPHP/index.php
@@ -0,0 +1,19 @@
+<?php
+
+// $nome = "Emerson";
+// $sobrenome = "Pinho \u{1F602}";
+
+// echo "$nome $sobrenome";
+
+// const ESTADO = "Paraíba";
+// echo "Eu adoro a " . ESTADO;
+
+ echo "Estamos no ano de ". date('Y');
+
+// $nome = "Emerson ";
+// $sobrenome = "Pinho";
+// $apelido = "Pinho dev";
+
+// echo "$apelido";
+
+?>
---
+++ exercicios/tiposPrimitivos/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos primitivos em PHP</title>
+</head>
+<body>
+    <h1>Teste de tipos primitivos</h1>
+    <?php 
+        // 0x = hexadecimal 0b = binario = octal
+        // $num = 010;
+        // echo "O valor da variavel é $num";
+
+        // $v = "Emerson";
+        // var_dump($v);
+
+        // $num = (integer)5e3; // 3 x 10(2) coerção
+        // echo "O valor é $num";
+        // var_dump($num);
+
+        // $num = (int)"950";
+        // var_dump($num);
+
+        $casado = false; // true:1. false é vazio
+        // var_dump($casado);
+        echo "O valor para casado é $casado";
+
+
+    ?>
+</body>
+</html>
